3e3c90d99fce7cd4346d789ad983e8610a567f2f
xgui4 <134389196+xgui4@users.noreply.github.com>
2025-05-25 14:05:19 -0400

    first build script for windows

---
+++ build.rs
@@ -0,0 +1,38 @@
+extern crate windows_exe_info;
+use windows_exe_info::versioninfo::*;
+
+fn main(){
+    windows_exe_info::icon::icon_ico("assets/avatar.ico");
+    VersionInfo {
+        file_version: Version(0, 0, 0, 1),
+        product_version: Version(0, 0, 0, 1),
+        file_flag_mask: FileFlagMask::Win16,
+        file_flags: FileFlags {
+            debug: false,
+            patched: false,
+            prerelease: false,
+            privatebuild: false,
+            infoinferred: false,
+            specialbuild: false,
+        },
+        file_os: FileOS::Windows32,
+        file_type: FileType::App,
+        file_info: vec![FileInfo {
+            lang: Language::USEnglish,
+            charset: CharacterSet::Multilingual,
+            comment: None,
+            company_name: "Xgui4 Studio".into(),
+            file_description: "Show an ASCII Art File in a terminal".into(),
+            file_version: "0.0.0.1".into(),
+            internal_name: "flex".into(),
+            legal_copyright: None,
+            legal_trademarks: None,
+            original_filename: "flex.exe".into(),
+            product_name: "Flex".into(),
+            product_version: "0.0.0.1".into(),
+            private_build: None,
+            special_build: None,
+        }],
+    }
+    .link().unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 pub mod program; 
+use std::io; 
+
 fn main()
 {
     program::start_program();
-    //println!("An error have occured")
+
+    println!("Press Enter to continue...");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
 }
